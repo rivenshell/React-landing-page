e0aea2e4a65f352019d7c47f82c2349d0662a2d4
Rivenshell <rivenshell111@gmail.com>
2022-11-13 22:59:35 -0500

    Structure

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+import navbar from "./component/navbar";
+import footer from "./component/footer";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
